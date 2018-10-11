65bf92e9af3bacb9f68b98e3dde4f94698b07305
Philip Fowler <philipwfowler@gmail.com>
2018-10-11 06:53:30 +0100

    fixed pkg_resources

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ setup(
     version='1.0.1',
     author='Philip W Fowler',
     packages=['gemucator'],
+    package_data={'gemucator': ['../config/H37Rv.gbk']},
     install_requires=[
         "biopython >= 1.70"
     ],
     scripts=["bin/gemucator-run.py"],
     license='unknown',
     long_description=open('README.md').read(),
+    zip_safe=False
 )
